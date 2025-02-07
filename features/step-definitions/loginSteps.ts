b423d215b59b8c3d80e176772c5c89e83d5b56d5
SerhiiQA <nabukve@gmail.com>
2025-02-07 14:52:35 +0200

    new

--- features/step-definitions/loginSteps.ts
+++ features/step-definitions/loginSteps.ts
@@ -1,16 +1,16 @@
 import { Given, When, Then } from '@cucumber/cucumber';
-import LoginPage from '../pageobjects/LoginPage';
+import loginPage from '../pageobjects/LoginPage';
 
 Given('User is located on the main page of saucedemo website', async () => {
-    await LoginPage.open();
+    await loginPage.open();
 });
 
 When('User click "Login" button', async () => {
-    await LoginPage.clickLogin();
+    await loginPage.clickLogin();
 });
 
 Then('User should see {string} error message', async (expectedMessage: string) => {
-    const errorMessage = await LoginPage.getErrorMessageText();
+    const errorMessage = await loginPage.getErrorMessageText();
     if (!errorMessage.includes(expectedMessage)) {
         throw new Error(`Expected "${expectedMessage}", got "${errorMessage}"`);
     }
