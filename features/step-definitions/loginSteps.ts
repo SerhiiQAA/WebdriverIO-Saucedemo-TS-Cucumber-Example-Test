8aad5e17edc3a467c9d5dc6144ef9f7786f6c91e
SerhiiQA <nabukve@gmail.com>
2025-02-07 14:35:21 +0200

    new

--- features/step-definitions/loginSteps.ts
+++ features/step-definitions/loginSteps.ts
@@ -1,5 +1,4 @@
 import { Given, When, Then } from '@cucumber/cucumber';
-import { expect } from '@wdio/globals';
 import LoginPage from '../pageobjects/LoginPage';
 
 Given('User is located on the main page of saucedemo website', async () => {
@@ -10,6 +9,13 @@ When('User click "Login" button', async () => {
     await LoginPage.clickLogin();
 });
 
-Then('User should see "Epic sadface: Username is required" error message', async () => {
-    expect(await LoginPage.getErrorMessageText()).toContain('Epic sadface: Username is required');
+// Then('User should see "Epic sadface: Username is required" error message', async () => {
+//     expect(await LoginPage.getErrorMessageText()).toContain('Epic sadface: Username is required');
+// });
+
+Then('User should see {string} error message', async (expectedMessage: string) => {
+    const errorMessage = await LoginPage.getErrorMessageText();
+    if (!errorMessage.includes(expectedMessage)) {
+        throw new Error(`Expected "${expectedMessage}", got "${errorMessage}"`);
+    }
 });
